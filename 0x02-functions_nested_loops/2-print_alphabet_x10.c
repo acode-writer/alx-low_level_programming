e47f73e73040b0571bbdf6568dabe7b18cd0948a
acode-writer <souhaibou.toure122@gmail.com>
2022-12-13 13:15:49 -0800

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,5 +10,11 @@ void print_alphabet_x10(void)
 	int times;
 
 	for (times = 0; times < 10; times++)
-		print_alphabet();
+	{
+		char letter;
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
 }
