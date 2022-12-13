654db26d0c75a86f542682c1948c7a9d89a2fbbe
acode-writer <souhaibou.toure122@gmail.com>
2022-12-13 13:09:58 -0800

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10: print alphabets 10 times.
+  * Return: void
+  */
+
+void print_alphabet_x10(void)
+{
+	int times;
+
+	for (times = 0; times < 10; times++)
+		print_alphabet();
+}
