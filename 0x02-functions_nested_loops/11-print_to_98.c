985f26cff9d4cc78cc32aa17cd24223413d56cc3
acode-writer <souhaibou.toure122@gmail.com>
2022-12-14 11:11:43 -0800

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * print_to_98 - print all number from n to 98.
+  * @n: an integer.
+  */
+void print_to_98(int n)
+{
+	if (n >= 98)
+		while (n > 98)
+			printf("%d, ", n--);
+	else
+		while (n < 98)
+			printf("%d, ", n++);
+	printf("%d\n", n);
+}
