efa4c088579cf1140b53519e8f974a623df8dceb
souhaiboutoure <souhaibou.toure122@gmail.com>
2023-01-02 06:22:15 -0800

    8. Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+/**
+ * set_string - set  the value of a pointer to a char
+ *
+ * @s: double pointer
+ * @to: pointer to set
+ *
+ * Description: this print the chessboard
+ * Return: a char
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
