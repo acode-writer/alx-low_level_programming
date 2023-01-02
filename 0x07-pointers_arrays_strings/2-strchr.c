f6ece5179e0795d529e6695610a42aa40c38ace6
souhaiboutoure <souhaibou.toure122@gmail.com>
2023-01-02 05:38:42 -0800

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+/**
+ * _strchr - locates a character in a string .
+ * @s: string to check
+ * @c: character to found
+ * Return: pointer to s or null
+ */
+
+char *_strchr(char *s, char c)
+{
+
+	while (*s)
+	{
+		s++;
+
+			if (*s == c)
+		{
+			return (s);
+		}
+
+
+	}
+
+		return (NULL);
+
+
+}
