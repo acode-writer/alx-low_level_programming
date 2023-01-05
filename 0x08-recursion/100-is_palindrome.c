5337ecb8b211c6c3ece50841d719b16e5d6c1d43
souhaiboutoure <souhaibou.toure122@gmail.com>
2023-01-05 11:41:26 -0800

    7. They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anythin

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,46 @@
+/**
+ * _compare - compare two strings
+ *
+ * @s: first part of string
+ * @p: last part of the string
+ * Return: 0 if not and 1 if equal
+ */
+int _compare(char *s, char *p)
+{
+	if (*s == '\0')
+		return (1);
+	if (*s == *p)
+		return (_compare(++s, --p));
+	else
+		return (0);
+}
+/**
+ * _reverse - this reverse the string to see if is the same
+ *
+ * @s: char pointer to check
+ *
+ * Return: return to the pointer in reverse
+ *
+ */
+char *_reverse(char *s)
+{
+	if (*s != '\0')
+		return (_reverse(++s));
+	else
+		return (--s);
+}
+/**
+ * is_palindrome - the given function reeturning extra functions
+ * _reverse - this will reverse the string
+ *
+ * @s: char pointer to check
+ *
+ * Return: 1 if is palindrome 0 if is not
+ */
+int is_palindrome(char *s)
+{
+	char *p;
+
+	p = _reverse(s);
+	return (_compare(s, p));
+}
